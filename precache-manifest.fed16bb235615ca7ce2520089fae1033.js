3e950795ddef2cd2a56d593fdd57031a5f97afad
Federico Marino <fede.m.fede@gmail.com>
2020-01-20 12:45:06 -0300

    Updates

---
+++ precache-manifest.fed16bb235615ca7ce2520089fae1033.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34f5fb43d22af07af8c741fbb5a963d",
+    "url": "/playing-flipping-card/index.html"
+  },
+  {
+    "revision": "4d15a27501e63a28e2b5",
+    "url": "/playing-flipping-card/static/css/main.3e697cd8.chunk.css"
+  },
+  {
+    "revision": "a87862eb1a0a990e539a",
+    "url": "/playing-flipping-card/static/js/2.6593997e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/playing-flipping-card/static/js/2.6593997e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d15a27501e63a28e2b5",
+    "url": "/playing-flipping-card/static/js/main.950ae792.chunk.js"
+  },
+  {
+    "revision": "52049a6b141fca76c926",
+    "url": "/playing-flipping-card/static/js/runtime-main.ad23248d.js"
+  },
+  {
+    "revision": "07be9e5147f6ae417b57d802804be949",
+    "url": "/playing-flipping-card/static/media/ace-of-spades.07be9e51.svg"
+  },
+  {
+    "revision": "80714de791fbd4e36b3c7c952826ceb3",
+    "url": "/playing-flipping-card/static/media/crown.80714de7.svg"
+  },
+  {
+    "revision": "25b4a4d68d670438af9e4981c62f49f5",
+    "url": "/playing-flipping-card/static/media/playingCardBackBackground.25b4a4d6.png"
+  }
+]);
