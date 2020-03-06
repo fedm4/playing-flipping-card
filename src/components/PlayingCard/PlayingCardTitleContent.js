72e15c1e21623d9c874393740d7ffdb2dbde45ae
Federico Marino <fede.m.fede@gmail.com>
2020-03-06 10:13:14 -0300

    test

--- src/components/PlayingCard/PlayingCardTitleContent.js
+++ src/components/PlayingCard/PlayingCardTitleContent.js
@@ -7,7 +7,7 @@ const PlayingCardTitleContent = props => {
     return (
         <div className="playing-card-title-content">
             <span className="playing-card-title-content-value">Q</span>
-            <h1 className="playing-card-title-content-title">Queen of Spades</h1>
+            <h1 className="playing-card-title-content-title">Queen!!! of Spades</h1>
             <div onClick={props.flip} className="playing-card-title-content-image-container">
                 <img className="playing-card-title-content-image" src={Spades} />
             </div>
